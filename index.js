c26ccade26cd4e9431ac5f3dd9acedc73b5b21ca
Wilder <wkerubo28@gmail.com>
2023-04-10 22:54:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 9
+let num4 = 5
+let mod = num3%num4
+let max;
+max = Math.max(10,20);
+
