46171b3bcf79bfd96f62823eb4d29c99590f7787
wondieki <wkerubo28@gmail.com>
2024-10-07 14:37:51 +0300

    Done

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-
-let num1 = 2
-let num2 = 31
-let multiply = num1 * num2
-let random = Math.floor(Math.random() * 10) + 1;
-let num3 = 9
-let num4 = 5
-let mod = num3%num4
-let max;
-max = Math.max(10,20);
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+const random = Math.round(Math.random() * 100) + 1;
+const num3 = 16;
+const num4 = 4;
+const mod = num3/num4;
+const max=Math.max(3,4,5,20)
 
